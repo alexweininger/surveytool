1e8dc863ae31d4ef1f3c6249d87bd72e1c9c8044
alexweininger <alex.weininger@live.com>
2018-04-11 13:01:13 -0700

    added comments to OptionResponse

--- src/surveyTool/OptionResponseQuestion.java
+++ src/surveyTool/OptionResponseQuestion.java
@@ -11,23 +11,24 @@ import java.util.ArrayList;
 
 public class OptionResponseQuestion extends ListResponseQuestion implements Display {
 
+	// constructors
 	public OptionResponseQuestion(String name, String questionText) {
-		super(name, questionText, 1);
+		super(name, questionText, 1); // set the response limit to 1 by default
 	}
 
 	public OptionResponseQuestion(String name, String questionText, ArrayList<String> options) {
-		super(name, questionText, 1, options);
+		super(name, questionText, 1, options); // set the response limit to 1 by default
 	}
 
-	public boolean isValidResponse(String resp) {
+	public boolean isValidResponse(String resp) { // is the user response valid, returns boolean
 		return true;
 	}
 
-	public String displayQuestion() {
+	public String displayQuestion() { // display the question in JPanel
 		return null;
 	}
 
-	public String toString() {
+	public String toString() { // toString
 		return null;
 	}
 
