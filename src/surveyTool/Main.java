58e68c4ffbc6bd480b35ddbaf56983a4cadc2f8f
alexweininger <alex.weininger@live.com>
2018-04-13 22:11:42 -0700

    initialized arraylists and added a small tester for the survey and question class in Main.java

--- src/surveyTool/Main.java
+++ src/surveyTool/Main.java
@@ -8,5 +8,9 @@ package surveyTool;
 
 public class Main {
 	public static void main(String[] args) {	//Main method to execute and test survey
+		Survey survey = new Survey("Survey Test", "Alex", true);
+		Question name = new Question("name question", "What is your name?");
+		survey.addQuestion(name);
+		survey.initSurvey();
 	}
 }
